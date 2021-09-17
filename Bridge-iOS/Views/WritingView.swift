7eec8044658bb025e76b29d780881fe23b337c53
twoj <45229285+JAEJOON-LEE@users.noreply.github.com>
2021-09-17 23:24:07 +0900

    Updated WritingView Contents #1

--- Bridge-iOS/Views/WritingView.swift
+++ Bridge-iOS/Views/WritingView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct WritingView : View {
     @State var contentTitle : String = ""
     @State var content : String = ""
+
+    @State var isOn1 = false
+    @State var isOn2 = false
     
     var body: some View {
         VStack(spacing : 20) {
@@ -25,10 +28,25 @@ struct WritingView : View {
             TextField("titleContent", text: $content)
                 .frame(height : UIScreen.main.bounds.height * 0.3)
                 .modifier(SignViewTextFieldStyle())
+            HStack{
+                Toggle("Anonymous", isOn: $isOn1)
+                Spacer()
+                Toggle("Bug Report", isOn: $isOn2)
+                Image(systemName: "camera").padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+            }
+            .padding(10)
+            .font(.caption)
+            
             Spacer()
+            
             Button("Upload") { }
                 .modifier(SubmitButtonStyle())
+            
             Spacer()
+            
+            // 탭 가려지는거 해결해야함
+            Text("asdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddasdsbgaddas")
+                .font(.caption2)
         }.padding()
     }
 }
