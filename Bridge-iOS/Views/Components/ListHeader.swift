2c8cff896a458cdc516357e29ecdaffc28db38c8
twoj <45229285+JAEJOON-LEE@users.noreply.github.com>
2021-09-18 13:14:52 +0900

    Added ListHeader for List style #1

--- Bridge-iOS/Views/Components/ListHeader.swift
+++ Bridge-iOS/Views/Components/ListHeader.swift
@@ -5,4 +5,24 @@
 //  Created by 이재준 on 2021/09/18.
 //
 
-import Foundation
+import SwiftUI
+
+struct ListHeader : View {
+    @State var name : String
+    
+    var body: some View {
+        HStack{
+            Text(name)
+                .foregroundColor(Color.yellow)
+                .font(.title2)
+                .padding()
+            Spacer()
+            Button(action: {}, label: {
+                Text("more")
+            })
+            .padding()
+        }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.05)
+        .background(Color.white)
+    }
+}
