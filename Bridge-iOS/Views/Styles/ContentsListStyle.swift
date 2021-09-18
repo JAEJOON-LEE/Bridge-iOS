c32b93784ca2a18aa60d7ec77b67db493621e276
twoj <45229285+JAEJOON-LEE@users.noreply.github.com>
2021-09-18 13:17:28 +0900

    Added ContentsListStyle for list items #1

--- Bridge-iOS/Views/Styles/ContentsListStyle.swift
+++ Bridge-iOS/Views/Styles/ContentsListStyle.swift
@@ -5,4 +5,16 @@
 //  Created by 이재준 on 2021/09/18.
 //
 
-import Foundation
+import SwiftUI
+
+struct ContentsListStyle : ViewModifier {
+    func body(content : Content) -> some View {
+        return content
+            .frame(width : UIScreen.main.bounds.width * 0.8)
+            .padding()
+            .foregroundColor(Color.white)
+            .background(Color.mainTheme)
+            .cornerRadius(20)
+            .shadow(radius: 3)
+    }
+}
